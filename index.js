36db48f5e1afb4ef4a8936431898794505fea131
Iago <iagofalcom@hotmail.com>
2019-04-03 12:49:00 -0300

    Adicionando o termo de busca e prefixo

---
+++ index.js
@@ -0,0 +1,25 @@
+const readline = require('readline-sync')
+
+function start() {
+	const content = {}
+
+	content.searchTerm = askAndReturnSearchTerm()
+	content.prefix = askAndReturnPrefix()
+
+	function askAndReturnSearchTerm(){
+		return readline.question('Escreva o termo para pesquisar no Wikipedia: ')
+	}
+
+	function askAndReturnPrefix(){
+		const prefixes = ['Quem e','O que e','A historia de']
+		const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao: ')
+		const selectedPrefixText = prefixes[selectedPrefixIndex]
+		
+		return selectedPrefixText
+
+	}
+
+	console.log(content)
+}
+
+start()
